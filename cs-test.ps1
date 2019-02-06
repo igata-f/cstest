1517cfd4837f339192aab906720eb9616befb1e3
igata-f <47382335+igata-f@users.noreply.github.com>
2019-02-06 20:16:51 +0900

    Update cs-test.ps1

--- cs-test.ps1
+++ cs-test.ps1
@@ -4,4 +4,5 @@ tasklist | Out-Null
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/igata-f/cstest/raw/master/cs_maltest.exe', "$env:temp"+'/cs_maltest.exe')
 .\cs_maltest.exe
+Start-Sleep -s 10
 Remove-Item cs_maltest.exe
